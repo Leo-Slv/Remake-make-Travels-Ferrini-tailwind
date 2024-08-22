8ed6c7a5946542e1f42c7093e924e460b1ee38c0
Leo-Slv <leonardo1692004@gmail.com>
2024-08-22 16:16:55 -0300

    Adição da chamada do cdn css do flowBite

--- src/header.php
+++ src/header.php
@@ -5,4 +5,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="./css/output.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css"  rel="stylesheet" />
 </head>
