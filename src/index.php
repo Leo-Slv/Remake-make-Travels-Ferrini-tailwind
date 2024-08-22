f1f62477b771ef30bfc5fae930ef668a0070ac0b
Leo-Slv <leonardo1692004@gmail.com>
2024-08-22 16:16:17 -0300

    Adição da chamada do cdn css do flowBite

--- src/index.php
+++ src/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="css/output.css" rel="stylesheet">
-    <link rel="stylesheet" href="./css/style.css">
+    <link href="./css/output.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css"  rel="stylesheet" />
 </head>
 
 <body>
@@ -24,6 +24,10 @@
   
 <?php
 require_once 'footer.php';
+?>
+
+<?php
+require_once 'script.php';
 ?>
 
 </body>
